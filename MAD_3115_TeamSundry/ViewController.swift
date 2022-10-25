836841d72e34f35949aed3d2c8e200e91006b478
Alice203 <c0874203@mylambton.ca>
2022-10-25 17:03:19 -0400

    Initial Commit

---
+++ MAD_3115_TeamSundry/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MAD_3115_TeamSundry
+//
+//  Created by Alice’z Poy on 2022-10-25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MAD_3115_TeamSundryUITests/MAD_3115_TeamSundryUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MAD_3115_TeamSundryUITestsLaunchTests.swift
+//  MAD_3115_TeamSundryUITests
+//
+//  Created by Alice’z Poy on 2022-10-25.
+//
+
+import XCTest
+
+final class MAD_3115_TeamSundryUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
