34e3d3e2713d4967bd8c89c6a69cbd88c2263b7d
BaoTrieuThai206 <c0866206@mylambton.ca>
2022-10-28 20:54:04 -0400

    Refactor Manager, Programmer & Permanent from OOP to POP

--- MAD_3115_TeamSundry/Files from MAD 3004/Work Space/Part 1/Programmer.swift
+++ MAD_3115_TeamSundry/Files from MAD 3004/Work Space/Part 1/Programmer.swift
@@ -5,46 +5,51 @@
 
 import Foundation
 
-class Programmer: Employee {
+protocol ProgrammerProtocol  {
+    var nbProjects: Int { get set }
+    var GainFactorProjects: Int { get }
+}
+
+struct Programmer: ProgrammerProtocol, EmployeeProtocol, EmployeeContract {
+    var name: String
+    var birthYear: Int
+    var age: Int
+    var monthlyIncome: Double
+    var rate: Int
+    var employeeVehicle: Vehicle?
+    var contract: ContractProtocol?
     
-    private var _nbProjects: Int=0
+    var nbProjects: Int = 0
+    let GainFactorProjects = 200
     
-    private let _GainFactorProjjects = 200
-    
-    var nbProjects: Int {
-        get { return _nbProjects }
-        set { _nbProjects = newValue }
+    var description: String {
+        let text = """
+         and completed \(nbProjects) projects.
+        His/Her estimated annual income is \(annualIncome())
+        """
+        return text
     }
     
-    func printMessage() {
-        printData("We have a new employee: \(name), a programmer");
+    init(name: String, birthYear: Int, nbProjects: Int, rate: Int = 100, employeeVehicle: Vehicle? = nil) {
+        self.name = name
+        self.birthYear = birthYear
+        self.age = 0
+        self.monthlyIncome = 0
+        self.rate = rate
+        self.employeeVehicle = employeeVehicle
+        self.nbProjects = nbProjects
+        printData(description)
     }
     
-    init(name: String, birthYear: Int,nbProjects: Int,rate:Int=100, employeeVehicle: Vehicle?=nil) {
-        super.init(name: name, birthYear: birthYear, rate: rate,employeeVehicle: employeeVehicle)
-        self._nbProjects = nbProjects;
-        printMessage()
+    func printData(_ msg :String){
+        print(msg)
     }
     
-    override func annualIncome() -> Double {
+    func annualIncome() -> Double {
         let baseYearlyIncome = (monthlyIncome * Double(12)) * Double(rate)/100
-        let bonus = Double(_GainFactorProjjects * nbProjects)
+        let bonus = Double(GainFactorProjects * nbProjects)
         return baseYearlyIncome + bonus
     }
 }
 
-extension Programmer {
-    override var description: String {
-//        let a =
-//        """
-//        Name: \(name), a Programmer \n
-//        """
-        
-        let b = """
-         and completed \(nbProjects) projects.
-        His/Her estimated annual income is \(annualIncome())
-        """
-        return super.description + b
-    }
-}
 
--- MAD_3115_TeamSundry/Files from MAD 3004/Work Space/Part 3/Permanent.swift
+++ MAD_3115_TeamSundry/Files from MAD 3004/Work Space/Part 3/Permanent.swift
@@ -4,49 +4,40 @@
 //
 
 import Foundation
+protocol PermanentProtocol  {
+    var nbChildren: Int { get set }
+    var married: Bool { get set }
+    var monthlySalary: Double { get set }
+    var bonusPerChildPerMonth: Int { get set }
+    var accumulatedDays: Int { get set }
+    
+}
 
-class Permanent:Contract {
+class Permanent: PermanentProtocol {
     
-    private var _nbChildren: Int = 0
-    private var _married: Bool = false
-    private var _monthlySalary: Double = 0.0
-    private var _bonusPerChildPerMonth: Int = 0
-    private var _accumulatedDays: Int = 0
+    var nbChildren: Int = 0
+    var married: Bool = false
+    var monthlySalary: Double = 0.0
+    var bonusPerChildPerMonth: Int = 0
+    var accumulatedDays: Int = 0
     
-    var nbChildren: Int {
-        get { return _nbChildren }
-        set { _nbChildren = newValue }
-    }
-    
-    var married: Bool {
-        get { return _married }
-        set { _married = newValue }
-    }
-    
-    var monthlySalary: Double {
-        get { return _monthlySalary }
-        set { _monthlySalary = newValue }
-    }
-    
-    var bonusPerChildPerMonth: Int {
-        get { return _bonusPerChildPerMonth }
-        set { _bonusPerChildPerMonth = newValue }
-    }
-    
-    var accumulatedDays: Int {
-        get { return _accumulatedDays }
-        set { _accumulatedDays = newValue }
+    var description: String {
+        let text = """
+        he \(married ? "is" : "is not") married and he/she has \(nbChildren) children.
+        He/She has worked for \(accumulatedDays) days and upon contract his/her monthly salary is \(monthlySalary).
+        """
+        return text
     }
     
     init(nbChildren: Int, married: Bool, monthlySalary: Double, bonusPerChildPerMonth: Int, accumulatedDays: Int) {
-        self._nbChildren = nbChildren
-        self._married = married
-        self._monthlySalary = monthlySalary
-        self._bonusPerChildPerMonth = bonusPerChildPerMonth
-        self._accumulatedDays = accumulatedDays
+        self.nbChildren = nbChildren
+        self.married = married
+        self.monthlySalary = monthlySalary
+        self.bonusPerChildPerMonth = bonusPerChildPerMonth
+        self.accumulatedDays = accumulatedDays
     }
     
-    override func accumulatedSalary() -> Double {
+    func accumulatedSalary() -> Double {
         var childBonus = 0
         var cumulativeSalary = 0.0
         if (married && nbChildren > 0){
@@ -55,13 +46,5 @@ class Permanent:Contract {
         let salary = monthlySalary + Double(childBonus)
         cumulativeSalary = (salary / 20) * Double(accumulatedDays)
         return cumulativeSalary
-    }
-    
-    override var description: String {
-        let a = """
-        he \(married ? "is" : "is not") married and he/she has \(nbChildren) children.
-        He/She has worked for \(accumulatedDays) days and upon contract his/her monthly salary is \(monthlySalary).
-        """
-        return super.description + a
     }
 }
