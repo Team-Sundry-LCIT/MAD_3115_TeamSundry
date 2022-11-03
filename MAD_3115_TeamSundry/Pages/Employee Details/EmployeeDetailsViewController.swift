18a496ac25c860985e4eb0be242bc4b67bf47e5f
Tilak <c0868747@mylambton.ca>
2022-11-03 18:37:45 -0400

    employee detail page design

--- MAD_3115_TeamSundry/Pages/Employee Details/EmployeeDetailsViewController.swift
+++ MAD_3115_TeamSundry/Pages/Employee Details/EmployeeDetailsViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class EmployeeDetailsViewController: UIViewController {
 
+    @IBOutlet weak var tvName: UILabel!
+    
+    @IBOutlet weak var tvType: UILabel!
+    
+    @IBOutlet weak var tvData: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,5 +31,30 @@ class EmployeeDetailsViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func populateData(){
+        
+        let a = ""
+        
+        tvName.text = "Tilak Acharya"
+        tvType.text = "An Android Developer"
+        
+        tvData.text = """
+        Name: \(a), a \(a)
+        Age : \(a)
+        
+        Employee has a \(a)
+        Model : \(a)
+        Plate : \(a)
+        Color : \(a)
+        With a side car
+        
+        Occupation rate : \(a)
+        Annual Income : \(a)
+        He/She has corrected
+        
+        """
+        
+    }
 
 }
