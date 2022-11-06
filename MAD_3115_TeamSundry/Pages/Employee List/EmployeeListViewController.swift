c10900820d5c29758c36e13bfb762012b2d323c3
BaoTrieuThai206 <c0866206@mylambton.ca>
2022-11-06 09:41:14 -0500

    Remove some class and fix bug

--- MAD_3115_TeamSundry/Pages/Employee List/EmployeeListViewController.swift
+++ MAD_3115_TeamSundry/Pages/Employee List/EmployeeListViewController.swift
@@ -9,8 +9,6 @@ class EmployeeListViewController: UIViewController {
 
     
     @IBOutlet weak var employeesListtableView: UITableView!
-    var employeesList  : [String] = ["Student A", "Student B", "Student C", "Student D", "Student E"]
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,14 +24,14 @@ extension EmployeeListViewController: UITableViewDelegate, UITableViewDataSource
         return 1
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return TableItems.names.count
+        return EmplyeeStruct.names.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = employeesListtableView.dequeueReusableCell(withIdentifier: "EmployeeCell") as? EmployeeListTableViewCell {
-            cell.nameLabel?.text = TableItems.names[indexPath.row]
-            cell.idLabel?.text = TableItems.ids[indexPath.row]
-            cell.avatarImage?.image = UIImage(named: TableItems.images[indexPath.row])
+            cell.nameLabel?.text = EmplyeeStruct.names[indexPath.row]
+            cell.idLabel?.text = EmplyeeStruct.ids[indexPath.row]
+            cell.avatarImage?.image = UIImage(named: EmplyeeStruct.images[indexPath.row])
             return cell
         }
         
@@ -41,16 +39,16 @@ extension EmployeeListViewController: UITableViewDelegate, UITableViewDataSource
     }
     
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        //return indexPath.row % 2 == 0 ? false : true
-        //=> cannot delete row 1,3,5,...
         return true
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            EmplyeeStruct.names.remove(at: indexPath.row)
+            EmplyeeStruct.ids.remove(at: indexPath.row)
+            EmplyeeStruct.images.remove(at: indexPath.row)
             
-            employeesList.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
---
+++ MAD_3115_TeamSundry/Pages/Employee List/EmplyeeStruct.swift
@@ -0,0 +1,14 @@
+//
+//  EmplyeeStruct.swift
+//  MAD_3115_TeamSundry
+//
+//  Created by Bao Trieu Thai on 2022-11-06.
+//
+
+import Foundation
+
+struct EmplyeeStruct {
+    static var names : [String] = ["Employee A","Employee B","Employee C","Employee D","Employee E","Employee F"]
+    static var ids : [String] = ["C001","C002","C003","C004","C005","C006"]
+    static var images : [String] = ["_Messages-avatar","_Messages-avatar","_Messages-avatar","_Messages-avatar","_Messages-avatar","_Messages-avatar"]
+}
